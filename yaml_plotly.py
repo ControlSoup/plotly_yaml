3922167c815e282c577831ea46c1f07ac484dcc5
Joe Wilson <your_email@example.com>
2023-10-29 10:13:48 -0700

    allowed saving to html with an output tag

--- yaml_plotly.py
+++ yaml_plotly.py
@@ -1,4 +1,4 @@
-#!/user/bin/python
+#!/usr/bin/python
 
 import argparse
 import os
@@ -36,7 +36,13 @@ def csv_to_datadict(file_path: str):
     return datadict
 
 
-def plot_from_yaml(group_name: str, plot_list: list, datadict: dict):
+def figs_from_yaml(
+    group_name: str,
+    plot_list: list,
+    datadict: dict
+) -> list[(str, go.Figure)]:
+
+    fig_list = []
 
     for plot_name, plot_contents in plot_list.items():
         fig = go.Figure()
@@ -80,13 +86,26 @@ def plot_from_yaml(group_name: str, plot_list: list, datadict: dict):
             )
             exit(1)
 
+        fig_title = f'{group_name}-{plot_name}'
         fig.update_layout(
-            title_text=f'{group_name} - {plot_name}',
+            title_text=fig_title,
             xaxis_title=x_key,
             yaxis_title=ylabel,
             showlegend=True
         )
-        fig.show()
+        fig_list.append((fig_title, fig))
+
+    return fig_list
+
+def output_fig_list(fig_list: list[(str, go.Figure)], output_path = None):
+    for tuple in fig_list:
+        name, fig = tuple
+
+        if output_path is None:
+            fig.show()
+        else:
+            fig.write_html(os.path.join(output_path, name + '.html'))
+
 
 def main():
 
@@ -97,12 +116,16 @@ def main():
     if not args.input:
         print(f"    ERROR| Please use -i for yaml config path")
         sys.exit(1)
-
-    if args.output:
-        output_path = os.path.join(abs_path, args.output)
-
     yaml_path = os.path.join(abs_path, args.input)
     yaml_dir = os.path.dirname(yaml_path)
+
+    output_path = None
+    if args.output:
+
+        if args.output == '.' or '' or 'current':
+            output_path = yaml_dir
+        else:
+            output_path = os.join(abs_path, args.output)
 
 
     # Parse Yaml
@@ -116,7 +139,6 @@ def main():
 
     # Check Errors
     if not DATA_PATH_KEY in yaml_dict:
-        datadict = csv_to_datadict(path)
         print(f'    ERROR| "{DATA_PATH_KEY}" field not found in yaml')
         exit(1)
 
@@ -131,11 +153,16 @@ def main():
     # Parse inputs
     data_path = yaml_dict[DATA_PATH_KEY]
     path = os.path.join(yaml_dir, data_path)
+    datadict = csv_to_datadict(path)
+
     group_name = yaml_dict[PLOT_NAME_KEY]
     plot_dict = yaml_dict[PLOT_LIST_KEY]
 
     # Plot the files
-    plot_from_yaml(group_name, plot_dict, datadict)
+    fig_list = figs_from_yaml(group_name, plot_dict, datadict)
+
+    # Output
+    output_fig_list(fig_list, output_path)
 
 if __name__ == '__main__':
     main()
