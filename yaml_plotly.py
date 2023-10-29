8b8e6886136b86399d5afc08d3c4f7eadf19e8ee
Joe Wilson <your_email@example.com>
2023-10-28 17:25:28 -0700

    updated formatting

--- yaml_plotly.py
+++ yaml_plotly.py
@@ -1,11 +1,11 @@
 #!/user/bin/python
 
-from plotly import graph_objects as go
 import argparse
 import os
 import pandas as pd
-from yaml import load
+from plotly import graph_objects as go
 import sys
+from yaml import load
 try:
     from yaml import CLoader as Loader
 except ImportError:
@@ -34,6 +34,7 @@ def csv_to_datadict(file_path: str):
     for key in df:
         datadict[key] = df[key].to_numpy()
     return datadict
+
 
 def plot_from_yaml(group_name: str, plot_list: list, datadict: dict):
 
@@ -72,6 +73,13 @@ def plot_from_yaml(group_name: str, plot_list: list, datadict: dict):
                         name=key
                     )
                 )
+        else:
+            print(
+                f'    ERROR| type field "{type}" it not supported please use:'
+                + '           [Line]'
+            )
+            exit(1)
+
         fig.update_layout(
             title_text=f'{group_name} - {plot_name}',
             xaxis_title=x_key,
@@ -85,16 +93,21 @@ def main():
     abs_path = os.getcwd()
 
     args = parse_args()
-    if args.input:
-        yaml_path = os.path.join(abs_path, args.input)
-        yaml_dir = os.path.dirname(yaml_path)
-    else:
+
+    if not args.input:
         print(f"    ERROR| Please use -i for yaml config path")
         sys.exit(1)
 
+    if args.output:
+        output_path = os.path.join(abs_path, args.output)
+
+    yaml_path = os.path.join(abs_path, args.input)
+    yaml_dir = os.path.dirname(yaml_path)
+
+
     # Parse Yaml
     yaml = open(yaml_path, 'r')
-    yaml_dict = load(yaml)
+    yaml_dict = load(yaml, Loader=Loader)
 
     # Globals
     DATA_PATH_KEY = "DataPath"
@@ -102,25 +115,24 @@ def main():
     PLOT_LIST_KEY = "Plots"
 
     # Check Errors
-    if DATA_PATH_KEY in yaml_dict:
-        data_path = yaml_dict[DATA_PATH_KEY]
-        path = os.path.join(yaml_dir, data_path)
+    if not DATA_PATH_KEY in yaml_dict:
         datadict = csv_to_datadict(path)
-    else:
         print(f'    ERROR| "{DATA_PATH_KEY}" field not found in yaml')
         exit(1)
 
-    if PLOT_NAME_KEY in yaml_dict:
-        group_name = yaml_dict[PLOT_NAME_KEY]
-    else:
+    if not PLOT_NAME_KEY in yaml_dict:
         print(f'    ERROR| "{PLOT_NAME_KEY}" field not found in yaml')
         exit(1)
 
-    if PLOT_LIST_KEY in yaml_dict:
-        plot_dict = yaml_dict[PLOT_LIST_KEY]
-    else:
+    if not PLOT_LIST_KEY in yaml_dict:
         print(f'    ERROR| "{PLOT_LIST_KEY}" field not found in yaml')
         exit(1)
+
+    # Parse inputs
+    data_path = yaml_dict[DATA_PATH_KEY]
+    path = os.path.join(yaml_dir, data_path)
+    group_name = yaml_dict[PLOT_NAME_KEY]
+    plot_dict = yaml_dict[PLOT_LIST_KEY]
 
     # Plot the files
     plot_from_yaml(group_name, plot_dict, datadict)
